3d62001db049cf61ccddd6f6b690992e12981e59
Ryuhei Kashi <manchester.ryu.121@gmail.com>
2019-11-14 12:13:25 +0900

    Create Category-f-l

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,31 @@ class Admin::CategoriesController < ApplicationController
   def index
     @users = User.paginate(page: params[:page], per_page: 10)
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Successfully Created !"
+      redirect_to admin_categories_url
+    else
+      flash.now[:danger] = "Invalid category"
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:title, :description)
+    end
 end
