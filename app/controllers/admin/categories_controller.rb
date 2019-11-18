e877da9aa5625e362fb61b66adfb60150b25f4f9
ryu0121 <56571304+ryu0121@users.noreply.github.com>
2019-11-18 11:09:08 +0900

    Edit-Delete function and layout (#11)
    * Edit-Delete function and layout
    
    * fixed confirmation

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,15 @@
 class Admin::CategoriesController < ApplicationController
+before_action :set_category, only: [:edit, :update, :destroy]
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 10)
   end
 
   def new
     @category = Category.new
+  end
+
+  def edit
   end
 
   def create
@@ -17,9 +22,28 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  def update
+    if @category.update(category_params)
+      flash[:success] = "Successfully Updated"
+      redirect_to admin_categories_url
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    flash[:success] = "Successfully deleted"
+    redirect_to admin_categories_url
+  end
+
   private
 
     def category_params
       params.require(:category).permit(:title, :description)
+    end
+
+    def set_category
+      @category = Category.find(params[:id])
     end
 end
