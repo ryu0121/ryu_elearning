3d613851979df6bd3cc51998f3c74865439568f8
ryu0121 <56571304+ryu0121@users.noreply.github.com>
2019-11-20 10:24:18 +0900

    user-restriction (#15)
    * user-restriction
    
    * user-restriction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    unless current_user.admin?
+      flash[:danger] = "You are not authorized."
+      redirect_to(root_url)
+    end
+  end
 end
