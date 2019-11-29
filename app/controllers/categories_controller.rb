aa35f6959d83cda858a94058d03047bcf54c0590
ryu0121 <56571304+ryu0121@users.noreply.github.com>
2019-11-29 11:07:16 +0900

    created word-learned-page (#21)
    * created word-learned-page
    
    * created word-learned and learned filter
    
    * fixed layout of follow-showpage
    
    * fixed color of button and indentions
    
    * fixed login_form falsh message
    
    * fixed state of button

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,11 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.paginate(page: params[:page], per_page: 10)
     @user = User.find(current_user.id)
+    @status = params[:status]
+    if @status == "1"
+      @categories = current_user.categories.paginate(page: params[:page], per_page: 10)
+    elsif @status == "0"
+        @categories = @categories.where.not(id: current_user.category_ids).paginate(page: params[:page], per_page: 10)
+    end
   end
 end
