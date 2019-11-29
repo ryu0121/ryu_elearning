75e9e892164ce3f901885edd14ae8d33aa62eccd
Ryuhei Kashi <manchester.ryu.121@gmail.com>
2019-11-29 09:13:37 +0900

    fixed color of button and indentions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,10 +3,10 @@ class CategoriesController < ApplicationController
     @categories = Category.paginate(page: params[:page], per_page: 10)
     @user = User.find(current_user.id)
     @status = params[:status]
-      if @status == "1"
-        @categories = current_user.categories.paginate(page: params[:page], per_page: 10)
-      elsif @status == "0"
-         @categories = @categories.where.not(id: current_user.category_ids).paginate(page: params[:page], per_page: 10)
-      end
+    if @status == "1"
+      @categories = current_user.categories.paginate(page: params[:page], per_page: 10)
+    elsif @status == "0"
+        @categories = @categories.where.not(id: current_user.category_ids).paginate(page: params[:page], per_page: 10)
+    end
   end
 end
