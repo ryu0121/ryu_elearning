f7751496b068c2e844f2a3295039c5bca8b92069
ryu0121 <56571304+ryu0121@users.noreply.github.com>
2019-11-29 13:15:27 +0900

    changed active-storage to carrierwave (#22)
    * changed active-storage to carrierwave
    
    * fixed one indention
    
    * fixed feed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_005500) do
+ActiveRecord::Schema.define(version: 2019_11_29_012555) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_005500) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "image"
   end
 
   create_table "words", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
